41e9c10d8f92bfed0208e8ea8c4f320ee96b5f47
Khanh Dao <kdao@Khanhs-MBP-2.attlocal.net>
2016-06-21 06:28:10 -0700

    Adding webapp handle localstorage

--- app/src/main/java/com/kdao/reesio_beta/MainActivity.java
+++ app/src/main/java/com/kdao/reesio_beta/MainActivity.java
@@ -7,8 +7,18 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.webkit.JavascriptInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mainWebView;
 
     /**
      * Called when the activity is first created.
@@ -17,9 +27,13 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+        mainWebView = (WebView) findViewById(R.id.mainWebView);
         WebSettings webSettings = mainWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled(true); //ENABLE JS
+        webSettings.setDomStorageEnabled(true); //enable DOM storage
+        //those two lines seem necessary to keep data that were stored even if the app was killed.
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDatabasePath(this.getFilesDir().getParentFile().getPath() + "/databases/");
         mainWebView.setWebViewClient(new MyCustomWebViewClient());
         mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         mainWebView.loadUrl("https://www.reesio.com/?mobile_override=true");
@@ -31,6 +45,104 @@ public class MainActivity extends AppCompatActivity {
             view.loadUrl(url);
             return true;
         }
+    }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mainWebView.canGoBack()) {
+            mainWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    //Substitute localStorage in Android webview
+    private class LocalStorageJavaScriptInterface {
+        private Context mContext;
+        private LocalStorage localStorageDBHelper;
+        private SQLiteDatabase database;
+
+        LocalStorageJavaScriptInterface(Context c) {
+            mContext = c;
+            localStorageDBHelper = LocalStorage.getInstance(mContext);
+        }
+
+        /**
+         * This method allows to get an item for the given key
+         * @param key : the key to look for in the local storage
+         * @return the item having the given key
+         */
+        @JavascriptInterface
+        public String getItem(String key) {
+            String value = null;
+            if(key != null)
+            {
+                database = localStorageDBHelper.getReadableDatabase();
+                Cursor cursor = database.query(LocalStorage.LOCALSTORAGE_TABLE_NAME,
+                        null,
+                        LocalStorage.LOCALSTORAGE_ID + " = ?",
+                        new String [] {key},null, null, null);
+                if(cursor.moveToFirst())
+                {
+                    value = cursor.getString(1);
+                }
+                cursor.close();
+                database.close();
+            }
+            return value;
+        }
+
+        /**
+         * set the value for the given key, or create the set of datas if the key does not exist already.
+         * @param key
+         * @param value
+         */
+        @JavascriptInterface
+        public void setItem(String key,String value) {
+            if(key != null && value != null) {
+                String oldValue = getItem(key);
+                database = localStorageDBHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(LocalStorage.LOCALSTORAGE_ID, key);
+                values.put(LocalStorage.LOCALSTORAGE_VALUE, value);
+                if(oldValue != null)
+                {
+                    database.update(LocalStorage.LOCALSTORAGE_TABLE_NAME, values, LocalStorage.LOCALSTORAGE_ID + "='" + key + "'", null);
+                }
+                else
+                {
+                    database.insert(LocalStorage.LOCALSTORAGE_TABLE_NAME, null, values);
+                }
+                database.close();
+            }
+        }
+
+        /**
+         * removes the item corresponding to the given key
+         * @param key
+         */
+        @JavascriptInterface
+        public void removeItem(String key) {
+            if(key != null)
+            {
+                database = localStorageDBHelper.getWritableDatabase();
+                database.delete(LocalStorage.LOCALSTORAGE_TABLE_NAME, LocalStorage.LOCALSTORAGE_ID + "='" + key + "'", null);
+                database.close();
+            }
+        }
+
+        /**
+         * clears all the local storage.
+         */
+        @JavascriptInterface
+        public void clear() {
+            database = localStorageDBHelper.getWritableDatabase();
+            database.delete(LocalStorage.LOCALSTORAGE_TABLE_NAME, null, null);
+            database.close();
+        }
     }
 }
