e0ac603458bd16be600c975671a006ca46cdedf3
Khanh Dao <kdao@Khanhs-MBP-2.attlocal.net>
2016-06-21 06:07:52 -0700

    Initial check in for reesio beta

--- app/src/main/java/com/kdao/reesio_beta/MainActivity.java
+++ app/src/main/java/com/kdao/reesio_beta/MainActivity.java
@@ -3,11 +3,34 @@ package com.kdao.reesio_beta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+        WebSettings webSettings = mainWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mainWebView.setWebViewClient(new MyCustomWebViewClient());
+        mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mainWebView.loadUrl("https://www.reesio.com/?mobile_override=true");
+    }
+
+    private class MyCustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
     }
 }
--- app/src/main/java/com/kdao/reesio_beta/SplashActivity.java
+++ app/src/main/java/com/kdao/reesio_beta/SplashActivity.java
@@ -3,11 +3,25 @@ package com.kdao.reesio_beta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
 public class SplashActivity extends AppCompatActivity {
+
+    //Splash Window keep 3 sec will into MainActivity
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new android.os.Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
     }
 }
